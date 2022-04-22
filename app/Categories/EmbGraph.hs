d3aa5002f5c33991a9a709b1387621277f47ff4a
rlou <rlou@americastg.com>
2022-04-22 14:46:23 -0300

    agora sim fiz o wrapper do jeito certo e tirei argumento de funcao

--- app/Categories/EmbGraph.hs
+++ app/Categories/EmbGraph.hs
@@ -1,13 +1,16 @@
 module Categories.EmbGraph (
     EmbGraph,
     createEmbGraph,
-    catToGraph,
+    toGraph,
     createEmblishArrow
 ) where
     import Categories.Category
     import Categories.SimpleGraph
     
-    type EmbGraph o a = Cat (EmbGraphObject o) (EmbGraphArrow o a)
+    data EmbGraph o a = EmbGraph {
+        cat ::Cat (EmbGraphObject o) (EmbGraphArrow o a),
+        toGraph :: [EmbGraphArrow o a] -> [Edge]
+    }
 
     data EmbGraphObject o = EmbGraphObject {
         tag :: String,
@@ -23,30 +26,33 @@ module Categories.EmbGraph (
     createEmblishArrow :: a -> String -> String -> EmbGraphArrow o a
     createEmblishArrow arr tagA tagB = EmbGraphArrow arr (EmbGraphObject tagA) (EmbGraphObject tagB)
 
-    createEmbGraph :: Cat o a -> EmbGraph o a
-    createEmbGraph cat = Cat (embGraphSource cat) (embGraphTarget cat) (embGraphId cat) (embGraphComp cat)
-        where
-            embGraphSource :: Cat o a -> EmbGraphArrow o a -> EmbGraphObject o
-            embGraphSource cat embArrow = tagSource embArrow $ source cat (arrow embArrow)
+    createEmbGraph :: Cat o a -> (o -> String, a -> String) -> EmbGraph o a
+    createEmbGraph cat toStringFunctor = 
+        EmbGraph 
+            (Cat (embGraphSource cat) (embGraphTarget cat) (embGraphId cat) (embGraphComp cat))
+            (catToGraph cat toStringFunctor)
+            where
+                embGraphSource :: Cat o a -> EmbGraphArrow o a -> EmbGraphObject o
+                embGraphSource cat embArrow = tagSource embArrow $ source cat (arrow embArrow)
 
-            embGraphTarget :: Cat o a -> EmbGraphArrow o a -> EmbGraphObject o
-            embGraphTarget cat embArrow = tagTarget embArrow $ target cat (arrow embArrow)
+                embGraphTarget :: Cat o a -> EmbGraphArrow o a -> EmbGraphObject o
+                embGraphTarget cat embArrow = tagTarget embArrow $ target cat (arrow embArrow)
 
-            embGraphId :: Cat o a -> EmbGraphObject o -> EmbGraphArrow o a
-            embGraphId cat obj = EmbGraphArrow (identity cat (embeddedObject obj)) (const obj) (const obj)
+                embGraphId :: Cat o a -> EmbGraphObject o -> EmbGraphArrow o a
+                embGraphId cat obj = EmbGraphArrow (identity cat (embeddedObject obj)) (const obj) (const obj)
 
-            embGraphComp :: Cat o a -> EmbGraphArrow o a -> EmbGraphArrow o a -> EmbGraphArrow o a
-            embGraphComp cat a b = EmbGraphArrow (camposition cat (arrow a) (arrow b)) (tagSource a) (tagTarget b)
+                embGraphComp :: Cat o a -> EmbGraphArrow o a -> EmbGraphArrow o a -> EmbGraphArrow o a
+                embGraphComp cat a b = EmbGraphArrow (camposition cat (arrow a) (arrow b)) (tagSource a) (tagTarget b)
     
-    catToGraph :: Cat o a -> (o -> String, a -> String) -> [EmbGraphArrow o a] -> [Edge]
-    catToGraph cat toStringFunctor = map arrowToEdge'
-        where arrowToEdge' = arrowToEdge cat toStringFunctor
+                catToGraph :: Cat o a -> (o -> String, a -> String) -> [EmbGraphArrow o a] -> [Edge]
+                catToGraph cat toStringFunctor = map arrowToEdge'
+                    where arrowToEdge' = arrowToEdge cat toStringFunctor
 
-    arrowToEdge :: Cat o a -> (o -> String, a -> String) -> EmbGraphArrow o a -> Edge
-    arrowToEdge cat (labelObject, labelArrow) EmbGraphArrow {arrow=arr, tagSource=tagSource, tagTarget=tagTarget} =
-        Edge (labelArrow arr) sourceNode targetNode
-            where sourceNode = objectToNode labelObject (tagSource (source cat arr))
-                  targetNode = objectToNode labelObject (tagTarget (target cat arr))
+                arrowToEdge :: Cat o a -> (o -> String, a -> String) -> EmbGraphArrow o a -> Edge
+                arrowToEdge cat (labelObject, labelArrow) EmbGraphArrow {arrow=arr, tagSource=tagSource, tagTarget=tagTarget} =
+                    Edge (labelArrow arr) sourceNode targetNode
+                        where sourceNode = objectToNode labelObject (tagSource (source cat arr))
+                              targetNode = objectToNode labelObject (tagTarget (target cat arr))
 
-    objectToNode :: (o -> String) -> EmbGraphObject o -> Node
-    objectToNode labelObject embGraph = Node (tag embGraph) (labelObject (embeddedObject embGraph))
+                objectToNode :: (o -> String) -> EmbGraphObject o -> Node
+                objectToNode labelObject embGraph = Node (tag embGraph) (labelObject (embeddedObject embGraph))
--- app/Main.hs
+++ app/Main.hs
@@ -6,27 +6,39 @@ import Categories.Finite
 import Categories.FinPoset
 import qualified Data.Set as Set
 
-finiteExample = simpleGraphToDot $ catToGraph finiteCat (show, show) [edgeF, edgeG, edgeH, edgeK]
+embFinitGraph :: EmbGraph FiniteObject  FiniteArrow 
+embFinitGraph = createEmbGraph finiteCat (show, show)
+
+finiteExample :: String
+finiteExample = simpleGraphToDot $ toGraph embFinitGraph [edgeF, edgeG, edgeH, edgeK]
     where edgeF = createEmblishArrow F "0" "1"
           edgeG = createEmblishArrow G "1" "3"
           edgeH = createEmblishArrow H "1" "3"
           edgeK = createEmblishArrow K "2" "3"
 
-finIntSetExample = simpleGraphToDot $ catToGraph createFinPosetCat (tagO, tagA) [edgeF, edgeG, edgeH]
-    where a = Set.fromList [0, 1, 2, 3]
+finIntSetExample :: String
+finIntSetExample = simpleGraphToDot $ toGraph embFinPosetInt  [edgeF, edgeG, edgeH]
+    where 
+          embFinPosetInt :: EmbGraph (FinPosetObjcet Int) (FinPosetArrow Int)
+          embFinPosetInt = createEmbGraph createFinPosetCat (tagO, tagA)
+          
+          a = Set.fromList [0, 1, 2, 3]
           f = constructFinPosetArrow a (\x -> x*x) Set.empty
           b = codomain f
           g = constructFinPosetArrow b (floor . sqrt . fromIntegral) (Set.fromList [0, 1])
           h = constructFinPosetArrow a (*3) (Set.fromList [5])
           c = codomain h
+          
           edgeF = createEmblishArrow f "1" "2"
           edgeG = createEmblishArrow g "2" "1"
           edgeH = createEmblishArrow h "1" "3"
+          
           tagO x 
                 | x == a = "a"
                 | x == b = "b"
                 | x == c = "c"
                 | otherwise = "Untaged"
+          
           tagA x 
                 | compareFinPosetArrow x f = "F"
                 | compareFinPosetArrow x g = "G"
@@ -34,4 +46,4 @@ finIntSetExample = simpleGraphToDot $ catToGraph createFinPosetCat (tagO, tagA) 
                 | otherwise = "Untaged"
 
 main = do  
-    putStrLn finIntSetExample
+    putStrLn finiteExample
